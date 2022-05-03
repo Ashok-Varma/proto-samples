7fcf3337dd83528b1784f8b43c67777c232130bd
Ashok Varma <ashokvitece@gmail.com>
2022-05-03 12:11:19 -0700

    fixing kotlin

--- tools/run_proto_kotlin.sh
+++ tools/run_proto_kotlin.sh
@@ -8,5 +8,6 @@ protoc-3.20.1-osx-aarch_64/bin/protoc \
 --plugin=protoc-gen-grpckt="protoc-gen-grpc-kotlin.sh" \
 --grpckt_out="../src/main/kotlin" \
 --java_out="../src/main/kotlin" \
+--kotlin_out="../src/main/kotlin" \
 ../src/main/proto/test2.proto \
 ../src/main/proto/test3.proto
--- tools/run_proto_kotlin_lite.sh
+++ tools/run_proto_kotlin_lite.sh
@@ -8,5 +8,6 @@ protoc-3.20.1-osx-aarch_64/bin/protoc \
 --plugin=protoc-gen-grpckt="protoc-gen-grpc-kotlin.sh" \
 --grpckt_out=lite:"../src/main/kotlin/lite" \
 --java_out=lite:"../src/main/kotlin/lite" \
+--kotlin_out=lite:"../src/main/kotlin/lite" \
 ../src/main/proto/test2.proto \
 ../src/main/proto/test3.proto
