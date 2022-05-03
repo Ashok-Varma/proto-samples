23e80dc4b0b4060e6a4b5340043c3591a37ec228
Ashok Varma <ashokvitece@gmail.com>
2022-05-03 11:59:06 -0700

    java-out with kotlin-out

--- tools/run_proto.sh
+++ tools/run_proto.sh
@@ -14,4 +14,5 @@ protoc-3.20.1-osx-aarch_64/bin/protoc \
 protoc-3.20.1-osx-aarch_64/bin/protoc \
 --proto_path="../src/main/proto" \
 --java_out="../src/main/java" \
+--kotlin_out="../src/main/java" \
 ../src/main/proto/dino.proto
